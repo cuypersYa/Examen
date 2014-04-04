8c54dc66972cf65adcae193c3d5b3994d08c0214
Yannick <yannickcuypers01@gmail.com>
2014-04-04 12:20:45 +0200

    start app + AuthorizeAttribute

---
+++ bugs-b-gone/bugs-b-gone/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(bugs_b_gone.Startup))]
+namespace bugs_b_gone
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
